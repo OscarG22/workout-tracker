dd06e2256a9af0d89358a9610354217e9b487f6b
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-12-13 01:33:54 -0700

    updated api routes

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,26 @@
 //week 17, activity 26
 
-// const router = require("express").Router();
-// const db = require("../models");
+const router = require("express").Router();
+const db = require("../models");
+const Workout = require("../models/workout");
 
-// router.post("/api/workout", ({ body }, res) => {
-//     Transaction.create(body)
-//         .then(dbTransaction => {
-//             res.json(dbTransaction);
-//         })
-//         .catch(err => {
-//             res.status(400).json(err);
-//         });
-// });
+//create workout
+router.post("/api/workout", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbworkout => {
+            res.json(dbworkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+//week 18, activity 9. Update workout
+app.put("/api/workout/:id", function (req, res) {
+    db.Workout.updateOne({ _id: req.params.id }).then(function (dbworkout) {
+        res.json(dbworkout);
+    });
+});
 
 
-
-module.exports = router;
+module.exports = router
